daf5ac0e4e5635de85313d397e7f4fcd99e21c0a
김재승 <jaes@gmail.com>
2025-02-18 15:30:47 +0900

    form데이터 받아오기 실패

--- std-02-18/app.js
+++ std-02-18/app.js
@@ -8,19 +8,37 @@ const server = http.createServer(function (request, response) {
         if (request.url === "/") {
             // console.log(pathbasename);
             console.log(request.url);
+            // request.on("data", function (data) {
+            //     console.log(data);
+            // });
+
             const mainpage = fs.readFileSync("index.html", "utf8");
             response.writeHead(200, { "content-type": "text/html" });
             response.end(mainpage);
         }
     }
     if (request.method === "POST") {
-        if (request.url === "/about") {
+        if (request.url === "/") {
             // console.log(pathbasename);
-            console.log(request.url.endsWith(""));
+            // console.log(request.url.endsWith(""));
+            request.on("data", function (data) {
+                fs.writeFileSync("star.txt", data.toString());
+                console.log(data);
+            });
             const mainpage = fs.readFileSync("about.html", "utf8");
             response.writeHead(200, { "content-type": "text/html" });
-            response.end(mainpage);
+            response.write(mainpage);
+            response.end();
         }
+        request.on("end", function () {
+            const resultString = fs.readFileSync("star.txt", "utf-8");
+            const resultSt = resultString.toString();
+            let qsData = qs.parse(resultSt);
+            let jsonTest = JSON.stringify(qsData);
+            fs.writeFileSync("star1.json", jsonTest);
+            console.log(qsData);
+            response.end(resultSt);
+        });
     }
 });
 
