5b608dd5de12868e2c7053b08db4c7aab44e196d
김재승 <jaes@gmail.com>
2025-02-18 12:47:21 +0900

    서버 구축

--- std-02-18/app.js
+++ std-02-18/app.js
@@ -1,1 +1,20 @@
-console.log("바뀐척");
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const pathbasename = path.basename();
+const server = http.createServer(function (request, response) {
+    if (request.method === "GET") {
+        if (request.url === "/") {
+            console.log(pathbasename);
+            const mainpage = fs.readFileSync("index.html", "utf8");
+            response.writeHead(200, { "content-type": "text/html" });
+            response.end;
+        }
+    }
+    if (request.method === "POST") {
+    }
+});
+
+server.listen(3000, () => {
+    console.log("3000번 서버 실행 http://localhost:3000");
+});
