c77ed1dd33dbfbf40d7bab70ccaa8f7d50378efd
김재승 <jaes@gmail.com>
2025-02-18 12:56:59 +0900

    form GET 요청 완료

--- std-02-18/app.js
+++ std-02-18/app.js
@@ -1,14 +1,23 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
-const pathbasename = path.basename();
+// const pathbasename = path.basename();
 const server = http.createServer(function (request, response) {
     if (request.method === "GET") {
+        console.log(request.url);
         if (request.url === "/") {
-            console.log(pathbasename);
+            // console.log(pathbasename);
+            console.log(request.url);
             const mainpage = fs.readFileSync("index.html", "utf8");
             response.writeHead(200, { "content-type": "text/html" });
-            response.end;
+            response.end(mainpage);
+        }
+        if (request.url === "/about?") {
+            // console.log(pathbasename);
+            console.log(request.url.endsWith(""));
+            const mainpage = fs.readFileSync("about.html", "utf8");
+            response.writeHead(200, { "content-type": "text/html" });
+            response.end(mainpage);
         }
     }
     if (request.method === "POST") {
