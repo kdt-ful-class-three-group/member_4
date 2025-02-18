4fb5fce825a19372c4e2edeeef33bc2ba1b5c240
김재승 <jaes@gmail.com>
2025-02-18 15:56:40 +0900

    form데이터받아오는중:

--- std-02-18/app.js
+++ std-02-18/app.js
@@ -1,44 +1,48 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
-// const pathbasename = path.basename();
+const qs = require("querystring");
 const server = http.createServer(function (request, response) {
     if (request.method === "GET") {
         console.log(request.url);
         if (request.url === "/") {
-            // console.log(pathbasename);
-            console.log(request.url);
-            // request.on("data", function (data) {
-            //     console.log(data);
-            // });
-
+            // console.log(request.url);
             const mainpage = fs.readFileSync("index.html", "utf8");
             response.writeHead(200, { "content-type": "text/html" });
             response.end(mainpage);
+            // } else if (request.url === "/about.html") {
+            //     const mainpage = fs.readFileSync("about.html", "utf8");
+            //     response.writeHead(200, { "content-type": "text/html" });
+            //     response.end(mainpage);
         }
     }
     if (request.method === "POST") {
         if (request.url === "/") {
-            // console.log(pathbasename);
-            // console.log(request.url.endsWith(""));
+            console.log(request.url);
+            const mainpage = fs.readFileSync("index.html", "utf8");
+            response.writeHead(200, { "content-type": "text/html" });
+            response.end(mainpage);
             request.on("data", function (data) {
-                fs.writeFileSync("star.txt", data.toString());
+                //request 데이터
+                fs.writeFileSync("index.html", data);
                 console.log(data);
             });
-            const mainpage = fs.readFileSync("about.html", "utf8");
-            response.writeHead(200, { "content-type": "text/html" });
-            response.write(mainpage);
-            response.end();
         }
-        request.on("end", function () {
-            const resultString = fs.readFileSync("star.txt", "utf-8");
-            const resultSt = resultString.toString();
-            let qsData = qs.parse(resultSt);
-            let jsonTest = JSON.stringify(qsData);
-            fs.writeFileSync("star1.json", jsonTest);
-            console.log(qsData);
-            response.end(resultSt);
-        });
+        // if (request.url === "/about") {
+        //     const mainpage = fs.readFileSync("about.html", "utf8");
+        //     request.on("data", function (data) {
+        //         //request 데이터
+        //         fs.writeFileSync("about.html", data);
+        //         console.log(data);
+        //     });
+        //     response.writeHead(200, { "content-type": "text/html" });
+        //     response.end(mainpage);
+        // }
+        // request.on("end", function () {
+        //     //data가 끝낫을경우 end
+        //     const aboutFile = fs.readFileSync("about.html");
+        //     response.end(aboutFile);
+        // });
     }
 });
 
